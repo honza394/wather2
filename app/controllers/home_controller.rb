758a2617ed60b7f3e502d86330e4ecd100fcb787
Jan Vacek <honza394@gmail.com>
2022-12-28 17:41:02 +0100

    add plumber example

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+  require "net/http"
+  require "uri"
+
   def index
+    uri = URI.parse("http://localhost:8000/echo?msg=hello") 
+    response = Net::HTTP.get_response(uri)
+    @hello = response.body
+
+    uri = URI("http://localhost:8000/sum")
+    response = Net::HTTP.post_form(uri, "a" => 5, "b" => 7)
+    #@sum = JSON.parse(response.body)
+    @sum = response.body
+
+    uri = URI("http://localhost:8000/viridis")
+    response = Net::HTTP.get_response(uri)
+
+    @viridis = response.body
+
   end
 end
